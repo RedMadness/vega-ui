4bb29c459dc9ab5b278509da14b202ad04e23469
Schegolev <bloodmoonxxx@gmail.com>
2026-05-12 13:27:55 +0300

    v0.6.34 fix(table)

--- src/components/VegaTable.vue
+++ src/components/VegaTable.vue
@@ -478,8 +478,10 @@ const selectedTableFilterValues = computed(() => {
 
 const requestParams = computed(() => {
   return {
-    ...props.filters,
-    ...selectedTableFilterValues.value,
+    filters: {
+      ...props.filters,
+      ...selectedTableFilterValues.value,
+    },
     ...props.remoteHandlerParams,
   }
 })
